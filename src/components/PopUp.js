054a9e54eab0873c85b7b1dab45ba4b8b6dd8219
Yumam20 <yuma.yuma.matsuoka@gmail.com>
2021-03-07 09:08:07 -0500

    with remain anonymous button
    title

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -39,6 +39,10 @@ const PopUp = (props) => {
     const updateInputValue = (evt) => {
         setInputValue(evt.target.value);
     };
+    const updateAnon = (evt) =>{
+        let val = "Tartan"
+        setInputValue(evt.target.val);
+    };
     return (
         <div className="modal" style = {styles.modal}>
 
@@ -55,6 +59,12 @@ const PopUp = (props) => {
                         
                         <label htmlFor="name_field">Name</label>
                         <input type="text" onChange={evt => updateInputValue(evt)} class="nes-input"/>
+                        <label>
+                            <label>
+                                <input type="checkbox" onChange= {evt => updateAnon(evt)} class="nes-checkbox is-dark"  />
+                            <span>Remain Anonymous </span>
+                            </label>
+                        </label>
 
                         <input type="submit" onClick={handleClick}/>
                     </div>
