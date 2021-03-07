c53940c037ada25f118514d304fe75acf213b90b
Joey Wildman <josephwildman88@gmail.com>
2021-03-07 10:53:13 -0500

    chnges

--- src/components/Mado.js
+++ src/components/Mado.js
@@ -9,11 +9,14 @@ import Bubble from "./Bubble.js";
 
 const Mado = (props) => {
     const [isSeen, setIsSeen] = React.useState(false);
-    const [userName,setName] = React.useState(props.value2 != 3 ? props.name : "No one");
+    console.log(props.value2)
+    
     const images = [working,sleeping,alive,empty];
-    const statuses = ["working","sleeping","alive","not here"];
+    const statuses = ["working","sleeping","vibing","not here"];
+    /*
+    const [userName,setName] = React.useState(props.value2 != 3 ? props.name : "No one");
     const [userStatus,setStatus] = React.useState(props.value2 != 3 ? statuses[props.value2] : "here");
-   
+   */
 
     const togglePop = () => {
         setIsSeen(!isSeen);
@@ -24,7 +27,7 @@ const Mado = (props) => {
     //const [currImage, setCurrImage] = React.useState(props.value2);
     return (
         <div>
-                {isSeen ? <Bubble name = {userName} statusMsg = {userStatus} />:null }
+                {isSeen ? <Bubble name = {props.name} statusMsg = {props.value2} />:null }
               <img src={images[props.value2]} alt="loading..." onClick={togglePop}/>  
         </div>
     
